7a3b7db475e46bb878154b476d5caceed31a8504
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-27 01:06:20 -0700

    menambahkan class pada pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -6,6 +6,18 @@ public:
     int umur;
 
     orang(int pUmur) : umur(pUmur){
-        
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
     }
+};
+
+class pekerja : virtual public orang {
+    public:
+
+    pekerja(int pUmur) : orang(pUmur){
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang{
+    
 }
