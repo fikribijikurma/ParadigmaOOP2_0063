80430d256225ec10f9321032ea5446a332732b6b
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-27 00:35:44 -0700

    menambahkan class pada joko

--- ParadigmaOOP2.cpp
+++ ParadigmaOOP2.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+        // cout << "pesan dari seseorang" << endl;
+    //}
+};
+
+class joko : public seseorang{
     
 }
